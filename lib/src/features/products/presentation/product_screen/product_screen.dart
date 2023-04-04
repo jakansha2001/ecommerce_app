da2cb7f22c10f31aba24f3708d2e619bd80cd2c3
jakansha2001 <jakansha2001@gmail.com>
2023-04-05 00:35:44 +0530

    refactor: :recycle: Fix lint issues

--- lib/src/features/products/presentation/product_screen/product_screen.dart
+++ lib/src/features/products/presentation/product_screen/product_screen.dart
@@ -1,6 +1,4 @@
 import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
-import 'package:ecommerce_app/src/common_widgets/error_message_widget.dart';
-import 'package:ecommerce_app/src/constants/test_products.dart';
 import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/utils/currency_formatter.dart';
--- lib/src/features/products/presentation/products_list/products_grid.dart
+++ lib/src/features/products/presentation/products_list/products_grid.dart
@@ -1,8 +1,6 @@
 import 'dart:math';
 
 import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
-import 'package:ecommerce_app/src/common_widgets/error_message_widget.dart';
-import 'package:ecommerce_app/src/constants/test_products.dart';
 import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/routing/app_router.dart';
